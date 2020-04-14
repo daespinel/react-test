0fbcf496c28bc5d3e9e67aa04996f8345d052a27
David Espinel <dafespinelsa@unal.edu.co>
2020-04-14 22:57:51 +0200

    Added docker-compose file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+	  Hi there, this is a change done to JS
+	  Following the example of volumes
         </p>
         <a
           className="App-link"
