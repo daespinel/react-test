d32ab6649325840fb07c84035a0cf636a4fb5247
David Espinel <dafespinelsa@unal.edu.co>
2021-01-12 20:17:14 +0100

    Modified travis file

--- src/App.test.js
+++ src/App.test.js
@@ -9,14 +9,14 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn react link', () => {
+test('renders learn react link2', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 
-test('renders learn react link', () => {
+test('renders learn react link3', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
