6e60df0c8ae57efc34748bd7fd232d7a6ddfa3ea
David Espinel <dafespinelsa@unal.edu.co>
2021-01-27 14:55:44 +0100

    Doing changes in feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Doing changes in the feature branch
+          Doing changes in the feature branch commiting
         </p>
         <a
           className="App-link"
